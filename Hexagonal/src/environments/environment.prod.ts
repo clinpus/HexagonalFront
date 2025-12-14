c1c1463a8a6d4bb439128cba28f5ff14012ab627
clinpus <ammari-youcef@hotmail.fr>
2025-12-14 18:06:23 +0100

    api  url env

--- Hexagonal/src/environments/environment.prod.ts
+++ Hexagonal/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   // URL de votre App Service Azure (HTTPS, car c'est une URL publique)
-  apiUrl: 'https://hexagon-hrezbcfmczbqdph0.francecentral-01.azurewebsites.net/api', // 
+  apiUrl: 'https://hexagon-hrezbcfmczbqdph0.francecentral-01.azurewebsites.net/api/', // 
   allowedOrigin: 'https://thankful-ocean-00c11ce03.3.azurestaticapps.net' // 
 };
